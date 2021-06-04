da86c2327a35ffdb90d2a2048187426cf308e48f
amirhachani <hachaniami@gmail.com>
2021-06-04 17:15:18 +0100

    props

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['.//client/*.{js,jsx,ts,tsx}', '../questionAndAnswers/public/index.html'],
+  purge: ['../questionAndAnswers/client/**/*.jsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
