57233552492dcde1966e37413195b9cf29cdefa9
amirhachani <hachaniami@gmail.com>
2021-06-04 17:12:18 +0100

    another

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', '../questionAndAnswers/public/index.html'],
+  purge: ['.//client/*.{js,jsx,ts,tsx}', '../questionAndAnswers/public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
